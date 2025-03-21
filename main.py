fd84df2f41d9668d1be21fbb0887ec06a6f911be
BondanTriWibowo06 <156983164+BondanTriWibowo06@users.noreply.github.com>
2025-03-21 22:04:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ def main():
         st.dataframe(df_day.head())
         st.write("### Data Jam")
         st.dataframe(df_hour.head())
-        st.write("### Info Dataset")
         st.text(df_day.info())
 
     elif menu == "Analisis Peminjaman Sepeda":
